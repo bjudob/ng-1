5c3d4ccdf06da491c39ac16eaf4cc44705d908b9
bjudob <konczbotondvagyok@gmail.com>
2026-06-02 23:03:43 +0300

    chore: add formatter addon

---
+++ scripts/main.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+enum Level {
+	# ui
+	MAIN_MENU,
+}
+
+@onready var level_to_scene = {
+	Level.MAIN_MENU: $MainMenu,
+		}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
