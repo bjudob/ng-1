2498aba6cd4047bcbca0d945b872bfcf5dc074fa
bjudob <konczbotondvagyok@gmail.com>
2026-06-08 00:12:04 +0300

    feat: game ui kickoff

--- level.gd
+++ level.gd
@@ -2,6 +2,7 @@ class_name Level
 extends Node2D
 
 @export var background_music = load("res://music/dark_orchestra.mp3")
+@export var isUI = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
