8b28f1d217a7f730e78ca2b5c94167cd489adcd8
bjudob <konczbotondvagyok@gmail.com>
2026-06-07 14:31:08 +0300

    feat: ghost movement basic

--- scripts/ghost_deer.gd
+++ scripts/ghost_deer.gd
@@ -2,24 +2,32 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const minOpacity = 0.3
+const maxOpacity = 0.5
+var opacityCounter = 0
+var opacityChange = 40
 
+@onready var sprite = $Sprite
+
+func _ready() -> void:
+	$Sprite.self_modulate.a = 0.5
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	# opacity
+	if opacityCounter % opacityChange == 0:
+		$Sprite.self_modulate.a = randf_range(minOpacity, maxOpacity)
+	opacityCounter += 1
+	
+	# move
+	var directionH := Input.get_axis("move_left", "move_right")
+	var directionV := Input.get_axis("move_up", "move_down")
+	if directionH:
+		velocity.x = directionH * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if directionV:
+		velocity.y = directionV * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
