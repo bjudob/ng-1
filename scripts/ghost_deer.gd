be3de0d709114c96c7c5976391cd21799bd5c42a
bjudob <konczbotondvagyok@gmail.com>
2026-06-07 21:15:48 +0300

    feat: cool ghost shader

--- scripts/ghost_deer.gd
+++ scripts/ghost_deer.gd
@@ -2,22 +2,14 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const minOpacity = 0.3
-const maxOpacity = 0.5
-var opacityCounter = 0
-var opacityChange = 40
 
 @onready var sprite = $Sprite
 
 func _ready() -> void:
-	$Sprite.self_modulate.a = 0.5
+	# $Sprite.self_modulate.a = 0.3
+	pass
 
 func _physics_process(delta: float) -> void:
-	# opacity
-	if opacityCounter % opacityChange == 0:
-		$Sprite.self_modulate.a = randf_range(minOpacity, maxOpacity)
-	opacityCounter += 1
-	
 	# move
 	var directionH := Input.get_axis("move_left", "move_right")
 	var directionV := Input.get_axis("move_up", "move_down")
