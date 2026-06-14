f4bb8777e2dfb9671f05acb50669c43954a31510
bjudob <konczbotondvagyok@gmail.com>
2026-06-14 16:31:13 +0300

    feat: black theme hand drawn and crazy rabbit guy

--- scripts/ghost_deer.gd
+++ scripts/ghost_deer.gd
@@ -3,10 +3,13 @@ extends CharacterBody2D
 const SPEED = 650.0
 
 @onready var sprite = $Sprite
+@onready var anim = $AnimationPlayer
+
 var movementTarget = Vector2()
 
 func _ready() -> void:
 	movementTarget = position
+	anim.play("ghost_deer_anim")
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("left_click"):
