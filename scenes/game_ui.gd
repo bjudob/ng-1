a3846212dbf2c26cadec66443d0184f872db2edf
bjudob <konczbotondvagyok@gmail.com>
2026-06-21 13:39:17 +0300

    feat: gameUI with word collecting v0.1

---
+++ scenes/game_ui.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+@onready var wordsContainer = $CanvasLayer/MarginContainer/WordsContainer
+
+var collectedWords = ["Cow", "Grass", "Cut"]
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	for label in wordsContainer.get_children():
+		wordsContainer.remove_child(label)
+	for word in collectedWords:
+		var label = Label.new()
+		label.text = word + "  "
+		var labelSettings = LabelSettings.new()
+		labelSettings.font_size = 50
+		label.label_settings = labelSettings
+		wordsContainer.add_child(label)
+
+func add_word(word: String) -> void:
+	collectedWords.append(word)
--- scripts/camera.gd
+++ scripts/camera.gd
@@ -7,17 +7,19 @@ const DISTANCE_TO_TRIGGER_MOVE = 500
 
 var halfScreen
 var moveTriggered
+var cameraHeight
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var screenSize = get_viewport().get_visible_rect().size
 	halfScreen = Vector2(screenSize.x/2, screenSize.y/2)
+	cameraHeight = player.position.y- screenSize.y/2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	var target = player.position
-	var fixedOnPlayer = target - halfScreen
+	var fixedOnPlayer = target - halfScreen - Vector2(0, cameraHeight)
 	var direction = (fixedOnPlayer - position).normalized()
 	
 	var onSide = abs(position.x - fixedOnPlayer.x) > DISTANCE_TO_TRIGGER_MOVE
@@ -29,5 +31,6 @@ func _process(_delta: float) -> void:
 	
 	if moveTriggered:
 		position.x = position.x + direction.x * SPEED
+		position.y = position.y + direction.y * SPEED
 	
 	
